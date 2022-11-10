98f466998b5cac86159554b0e13b2139583d8890
Chebanenko Sevindzh <se8a16@mail.ru>
2022-11-10 10:49:47 +0500

    cicle for in

--- app.js
+++ app.js
@@ -15,3 +15,13 @@ console.log(options.name)
 delete options.name
 
 console.log(options)
+
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+        console.log(`Свойство ${i} имеет значение ${options[key][i]}`)
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`)
+    }
+}
